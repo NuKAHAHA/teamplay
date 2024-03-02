71de1cde649b506065ae20d824c7a3e5d11ac875
git config --global user.name <146062616+NuKAHAHA@users.noreply.github.com>
2024-03-02 22:18:14 +0600

    test for me

--- main.go
+++ main.go
@@ -3,8 +3,7 @@ import (
 "log"
 "net/http"
 )
-// Define a home handler function which writes a byte slice containing
-// "Hello from Snippetbox" as the response body.
+
 func home(w http.ResponseWriter, r *http.Request) {
 w.Write([]byte("Hello from Snippetbox"))
 }
